b9fceefbb15f021e694e194e5c5d555580fb8874
t-ryo <t.ryosuke.fv@gmail.com>
2019-01-21 15:24:11 +0900

    modified path

--- arare/run.py
+++ arare/run.py
@@ -2,7 +2,7 @@
 #
 #
 #
-
+import subprocess, platform
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -12,4 +12,17 @@ def index():
 
 if __name__ == '__main__':
 	app.debug = True
+	if platform.system() == 'Darwin':
+		try:
+			subprocess.check_call(['open', 'http://localhost:8080'])
+			pass
+		except:
+			pass
+	elif platform.system() == 'Windows':
+		try:
+			subprocess.check_call(['start', 'http://localhost:8080'])
+			pass
+		except:
+			pass
+
 	app.run(host='0.0.0.0', port=8080)
--- arare/static/editor.js
+++ arare/static/editor.js
@@ -5,7 +5,7 @@
   var editor = ace.edit("editor");
   editor.setTheme("ace/theme/solarized_light");
   editor.getSession().setUseWrapMode(true);/* 折り返しあり */
-  //editor.setFontSize(24);
+  editor.setFontSize(24);
 
   var timer = null;
   editor.on("change", function (cm, obj) {
