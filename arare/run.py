3cc82f32c8f2bf0bd3141b9c308ce93b085a2d37
Kimio Kuramitsu <kkuramitsu@gmail.com>
2019-03-26 14:13:15 +0900

    added transpile() in run.py

--- arare/run.py
+++ arare/run.py
@@ -3,7 +3,11 @@
 #
 #
 
-from flask import Flask, render_template, send_file
+# comment out
+# from pegpy.origami.arare import compile
+
+from flask import Flask, render_template, send_file, request, Response
+
 app = Flask(__name__, template_folder='front/static')
 
 @app.route('/')
@@ -17,6 +21,15 @@ def css():
 @app.route('/<path:d>')
 def dist(d):
 	return render_template(d)
+
+@app.route('/compile', methods=['POST'])
+def transcompile():
+    inputText = request.form['source']
+    if '(ArareCode)' in inputText:
+            code = inputText
+    else:
+            code = compile(inputText)
+    return Response(code, mimetype='application/javascript')
 
 def main():
 	app.run(host='0.0.0.0', port=8080, debug=True)
