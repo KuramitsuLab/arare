0a4703df6b258947c5df5a4f4dd3511e85c42f50
tadataku-lab <tadataku.lab@gmail.com>
2019-06-14 17:08:26 +0900

    fix: set ArareCode for dev mode

--- puppy/client/server.js
+++ puppy/client/server.js
@@ -1,21 +1,22 @@
 var express = require("express");
+var bodyParser = require('body-parser');
 var path = require("path");
 var app = express();
+
+app.use(bodyParser.text({type: 'application/text'}));
 
 var server = app.listen(8080, function () {
   console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
+app.get("/sample/:path/:path", function (req, res, next) {
+  res.sendFile(path.join(__dirname, './static/sample/js/PuppyVMCode.js'));
+});
+
 app.use(express.static(path.join(__dirname, 'static')));
 
-app.get("/", function (req, res, next) {
-  res.render("index.html");
-});
-
-app.get("/:kind(audio|image|js|sample)/:path", function (req, res, next) {
-  res.sendFile(path.join(__dirname, `./static/${req.params['kind']}/${req.params['path']}`));
-});
 
 app.post("/compile", function (req, res, next) {
-  res.sendFile(path.join(__dirname, './static/sample/js/PuppyVMCode.js'));
+  res.setHeader('Content-Type', 'application/javascript');
+  res.send(req.body);
 });
