711147b23b8b3c824cc7da2950fcb633e5dbde06
tadataku-lab <tadataku.lab@gmail.com>
2019-06-16 15:39:14 +0900

    fet: able to customize

--- puppy/client/src/view/editor.ts
+++ puppy/client/src/view/editor.ts
@@ -1,7 +1,8 @@
 import * as ace from '../../node_modules/ace-builds/src-min-noconflict/ace.js';
-import * as solarized_light from '../../node_modules/ace-builds/src-min-noconflict/theme-solarized_light.js';
+// import * as solarized_light from '../../node_modules/ace-builds/src-min-noconflict/theme-solarized_light.js';
 import * as python_mode from '../../node_modules/ace-builds/src-min-noconflict/mode-python.js';
 // import * as js_mode from '../../node_modules/ace-builds/src-min-noconflict/mode-javascript.js';
+import * as $ from 'jquery';
 
 const editorState = {
   fontSize: 20,
@@ -22,8 +23,10 @@ export const fontMinus = () => {
 };
 
 export const editor = ace.edit('editor');
-editor.setTheme(solarized_light);
+// editor.setTheme(solarized_light);
 editor.getSession().setMode(new python_mode.Mode());
 // editor.getSession().setMode(new js_mode.Mode());
 editor.getSession().setUseWrapMode(true); /* 折り返しあり */
 setFontSize(20);
+
+$('.ace_gutter').css({ background: 'rgb(49,48,47)', color: 'white' });
